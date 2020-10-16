7763c20b02b91b6102e9ae4092f64e5e59fa3e35
Jules <jules.sab132@outlook.fr>
2020-10-16 11:28:44 +0200

    oscour

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -12,8 +12,8 @@ use Doctrine\ORM\Mapping as ORM;
  * @ApiResource(
  *     attributes={"security"="is_granted('ROLE_USER')"},
  *     collectionOperations={
- *         "get"={"security"="is_granted('ROLE_ADMIN') or object.owner == user"},
- *         "post"={"security"="is_granted('ROLE_ADMIN') or object.owner == user"}
+ *         "get",
+ *         "post"
  *     },
  *     itemOperations={
  *         "get"={"security"="is_granted('ROLE_ADMIN') or object.owner == user"},
